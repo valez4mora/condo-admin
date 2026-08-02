bd90678d86206dc81b10d10cf0f9b56dc25be866
valez4mora <zamorasotovaleria@gmail.com>
2026-08-01 19:02:52 -0600

    Gestión de Propiedades

---
+++ Util/Validaciones/CheckPropiedad.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Util.Validaciones
+{
+    internal class CheckPropiedad
+    {
+    }
+}
