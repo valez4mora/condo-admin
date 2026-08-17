db69665673e7bd26326b23f1114549c1509a0989
DANA ISABELLA DUARTE LINARES <daduarteli@est.utn.ac.cr>
2026-08-16 22:55:13 -0600

    Fibalizacion del DAo , usuario y rol

--- DAL/DAO/RolDAO.cs
+++ DAL/DAO/RolDAO.cs
@@ -1,12 +1,165 @@
-﻿using System;
+﻿using BLL;
+using DAL.DAO;
+using DAL.Singleton;
+using DTO;
+using Entities;
+using Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Util;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace DAL.DAO
 {
-    internal class RolDAO
+    public class RolDAO : IRolDAL
     {
+
+
+        public int Registrar(RolDTO rol)
+        {
+        
+            using (SqlConnection cn = Conexion.Instancia.ObtenerConexion())
+            {
+                cn.Open();
+                using (SqlCommand cmd = new SqlCommand("sp_RegistrarRol", cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Nombre", rol.Nombre);
+                    cmd.Parameters.AddWithValue("@Descripcion", (object)rol.Descripcion ?? DBNull.Value);
+                    object resultado = cmd.ExecuteScalar();
+                    return Convert.ToInt32(resultado);
+                }
+            }
+        }
+
+        public List<RolDTO> ObtenerTodos()
+        {
+            List<RolDTO> lista = new List<RolDTO>();
+
+            using (SqlConnection cn = Conexion.Instancia.ObtenerConexion())
+            {
+                cn.Open();
+                using (SqlCommand cmd = new SqlCommand("sp_ObtenerRoles", cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(new RolDTO
+                            {
+                                IdRol = Convert.ToInt32(dr["IdRol"]),
+                                Nombre = dr["Nombre"].ToString(),
+                                Descripcion = dr["Descripcion"] == DBNull.Value
+                                              ? string.Empty
+                                              : dr["Descripcion"].ToString()
+                            });
+                        }
+                    }
+                }
+            }
+            return lista;
+        }
+
+
+        public RolDTO ObtenerPorId(int idRol)
+        { 
+            return ObtenerTodos().Find(r => r.IdRol == idRol);
+        }
+
+        public bool Modificar(RolDTO rol)
+        {
+            using (SqlConnection cn = Conexion.Instancia.ObtenerConexion())
+            {
+                cn.Open();
+                using (SqlCommand cmd = new SqlCommand("sp_ModificarRol", cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@IdRol", rol.IdRol);
+                    cmd.Parameters.AddWithValue("@Nombre", rol.Nombre);
+                    cmd.Parameters.AddWithValue("@Descripcion", (object)rol.Descripcion ?? DBNull.Value);
+
+                    // ExecuteNonQuery retorna filas afectadas; > 0 significa éxito
+                    return cmd.ExecuteNonQuery() > 0;
+                }
+            }
+        }
+
+        
+        public bool Eliminar(int idRol)
+        {
+      
+            using (SqlConnection cn = Conexion.Instancia.ObtenerConexion())
+            {
+                cn.Open();
+                using (SqlCommand cmd = new SqlCommand("sp_EliminarRol", cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@IdRol", idRol);
+                    cmd.ExecuteNonQuery();
+                    return true;
+                }
+            }
+        }
+
+        public List<PermisoDTO> ObtenerPermisos(int idRol)
+        {
+            List<PermisoDTO> lista = new List<PermisoDTO>();
+
+            using (SqlConnection cn = Conexion.Instancia.ObtenerConexion())
+            {
+                cn.Open();
+                using (SqlCommand cmd = new SqlCommand("sp_ObtenerPermisosPorRol", cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@IdRol", idRol);
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(new PermisoDTO
+                            {
+                                IdPermiso = Convert.ToInt32(dr["IdPermiso"]),
+                                IdRol = Convert.ToInt32(dr["IdRol"]),
+                                Modulo = dr["Modulo"].ToString(),
+                                PuedeVer = Convert.ToBoolean(dr["PuedeVer"]),
+                                PuedeCrear = Convert.ToBoolean(dr["PuedeCrear"]),
+                                PuedeEditar = Convert.ToBoolean(dr["PuedeEditar"]),
+                                PuedeEliminar = Convert.ToBoolean(dr["PuedeEliminar"])
+                            });
+                        }
+                    }
+                }
+            }
+            return lista;
+        }
+
+ 
+        public bool GuardarPermiso(PermisoDTO permiso)
+        {
+            using (SqlConnection cn = Conexion.Instancia.ObtenerConexion())
+            {
+                cn.Open();
+                using (SqlCommand cmd = new SqlCommand("sp_GuardarPermisos", cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@IdRol", permiso.IdRol);
+                    cmd.Parameters.AddWithValue("@Modulo", permiso.Modulo);
+                    cmd.Parameters.AddWithValue("@PuedeVer", permiso.PuedeVer);
+                    cmd.Parameters.AddWithValue("@PuedeCrear", permiso.PuedeCrear);
+                    cmd.Parameters.AddWithValue("@PuedeEditar", permiso.PuedeEditar);
+                    cmd.Parameters.AddWithValue("@PuedeEliminar", permiso.PuedeEliminar);
+                    cmd.ExecuteNonQuery();
+                    return true;
+                }
+            }
+        }
     }
 }
--- DAL/DAO/UsuarioDAO.cs
+++ DAL/DAO/UsuarioDAO.cs
@@ -1,12 +1,150 @@
-﻿using System;
+﻿using DAL.Singleton;
+using DTO;
+using Interfaces;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace DAL.DAO
 {
-    internal class UsuarioDAO
+    
+    public class UsuarioDAO : IUsuarioDAL
     {
+       
+        public int Registrar(UsuarioDTO usuario)
+        {
+            using (SqlConnection cn = Conexion.Instancia.ObtenerConexion())
+            {
+                cn.Open();
+                using (SqlCommand cmd = new SqlCommand("sp_RegistrarUsuario", cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Usuario", usuario.Usuario);
+                    cmd.Parameters.AddWithValue("@ContrasenaHash", usuario.Contrasena); // ya hasheada
+                    cmd.Parameters.AddWithValue("@IdRol", usuario.IdRol);
+                    cmd.Parameters.AddWithValue("@Estado", usuario.Estado);
+
+                    object resultado = cmd.ExecuteScalar();
+                    return Convert.ToInt32(resultado);
+                }
+            }
+        }
+
+     
+        public List<UsuarioDTO> ObtenerTodos()
+        {
+            List<UsuarioDTO> lista = new List<UsuarioDTO>();
+
+            using (SqlConnection cn = Conexion.Instancia.ObtenerConexion())
+            {
+                cn.Open();
+                using (SqlCommand cmd = new SqlCommand("sp_ObtenerUsuarios", cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(new UsuarioDTO
+                            {
+                                IdUsuario = Convert.ToInt32(dr["IdUsuario"]),
+                                Usuario = dr["Usuario"].ToString(),
+                                Contrasena = string.Empty, // no se le de vuelve el hash a la ui
+                                Estado = Convert.ToBoolean(dr["Estado"]),
+                                IdRol = Convert.ToInt32(dr["IdRol"]),
+                                NombreRol = dr["NombreRol"].ToString()
+                            });
+                        }
+                    }
+                }
+            }
+            return lista;
+        }
+
+        
+        public UsuarioDTO ObtenerPorCredenciales(string usuario, string hashContrasena)
+        {
+ 
+            using (SqlConnection cn = Conexion.Instancia.ObtenerConexion())
+            {
+                cn.Open();
+                using (SqlCommand cmd = new SqlCommand("sp_ObtenerUsuarioPorCredenciales", cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Usuario", usuario);
+                    cmd.Parameters.AddWithValue("@ContrasenaHash", hashContrasena);
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        if (dr.Read())
+                        {
+                            return new UsuarioDTO
+                            {
+                                IdUsuario = Convert.ToInt32(dr["IdUsuario"]),
+                                Usuario = dr["Usuario"].ToString(),
+                                Contrasena = string.Empty, // no devolver hash
+                                Estado = Convert.ToBoolean(dr["Estado"]),
+                                IdRol = Convert.ToInt32(dr["IdRol"]),
+                                NombreRol = dr["NombreRol"].ToString()
+                            };
+                        }
+                        return null; 
+                    }
+                }
+            }
+        }
+
+        
+        public bool Modificar(UsuarioDTO usuario)
+        {
+            using (SqlConnection cn = Conexion.Instancia.ObtenerConexion())
+            {
+                cn.Open();
+                using (SqlCommand cmd = new SqlCommand("sp_ModificarUsuario", cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@IdUsuario", usuario.IdUsuario);
+                    cmd.Parameters.AddWithValue("@Usuario", usuario.Usuario);
+                    cmd.Parameters.AddWithValue("@IdRol", usuario.IdRol);
+                    cmd.Parameters.AddWithValue("@Estado", usuario.Estado);
+                    return cmd.ExecuteNonQuery() > 0;
+                }
+            }
+        }
+
+      
+        public bool CambiarContrasena(int idUsuario, string nuevoHash)
+        {
+            using (SqlConnection cn = Conexion.Instancia.ObtenerConexion())
+            {
+                cn.Open();
+                using (SqlCommand cmd = new SqlCommand("sp_CambiarContrasena", cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@IdUsuario", idUsuario);
+                    cmd.Parameters.AddWithValue("@ContrasenaHash", nuevoHash);
+                    return cmd.ExecuteNonQuery() > 0;
+                }
+            }
+        }
+
+       
+        public bool Eliminar(int idUsuario)
+        {
+    
+            using (SqlConnection cn = Conexion.Instancia.ObtenerConexion())
+            {
+                cn.Open();
+                using (SqlCommand cmd = new SqlCommand("sp_EliminarUsuario", cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@IdUsuario", idUsuario);
+                    cmd.ExecuteNonQuery();
+                    return true;
+                }
+            }
+        }
     }
 }
